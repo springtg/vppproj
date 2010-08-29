699b4654bd0f8af3a69cf3c0e3dec3860338f007
tqtrang07 <tqtrang07@4f51cf1c-0b8c-b79f-ed1e-ba211a99c7d2>
2010-08-29 08:19:38 +0000

    Complete

--- QLKHO/QLKHO/FORM/MANAGEMENT/frmGroupItem.Designer.cs
+++ QLKHO/QLKHO/FORM/MANAGEMENT/frmGroupItem.Designer.cs
@@ -33,20 +33,20 @@
             this.bar2 = new DevExpress.XtraBars.Bar();
             this.barButtonItem1 = new DevExpress.XtraBars.BarButtonItem();
             this.barButtonItem2 = new DevExpress.XtraBars.BarButtonItem();
+            this.btnDelete = new DevExpress.XtraBars.BarButtonItem();
             this.barDockControlTop = new DevExpress.XtraBars.BarDockControl();
             this.barDockControlBottom = new DevExpress.XtraBars.BarDockControl();
             this.barDockControlLeft = new DevExpress.XtraBars.BarDockControl();
             this.barDockControlRight = new DevExpress.XtraBars.BarDockControl();
-            this.barButtonItem3 = new DevExpress.XtraBars.BarButtonItem();
-            this.gridControl1 = new DevExpress.XtraGrid.GridControl();
+            this.grdGroup = new DevExpress.XtraGrid.GridControl();
             this.gridView1 = new DevExpress.XtraGrid.Views.Grid.GridView();
             this.gridColumn1 = new DevExpress.XtraGrid.Columns.GridColumn();
             this.gridColumn2 = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.repositoryItemLookUpEdit1 = new DevExpress.XtraEditors.Repository.RepositoryItemLookUpEdit();
             this.gridColumn3 = new DevExpress.XtraGrid.Columns.GridColumn();
             this.gridColumn4 = new DevExpress.XtraGrid.Columns.GridColumn();
-            this.repositoryItemLookUpEdit1 = new DevExpress.XtraEditors.Repository.RepositoryItemLookUpEdit();
             ((System.ComponentModel.ISupportInitialize)(this.barManager1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.grdGroup)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.gridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.repositoryItemLookUpEdit1)).BeginInit();
             this.SuspendLayout();
@@ -63,7 +63,7 @@
             this.barManager1.Items.AddRange(new DevExpress.XtraBars.BarItem[] {
             this.barButtonItem1,
             this.barButtonItem2,
-            this.barButtonItem3});
+            this.btnDelete});
             this.barManager1.MainMenu = this.bar2;
             this.barManager1.MaxItemId = 3;
             // 
@@ -74,9 +74,9 @@
             this.bar2.DockRow = 0;
             this.bar2.DockStyle = DevExpress.XtraBars.BarDockStyle.Top;
             this.bar2.LinksPersistInfo.AddRange(new DevExpress.XtraBars.LinkPersistInfo[] {
-            new DevExpress.XtraBars.LinkPersistInfo(this.barButtonItem1),
-            new DevExpress.XtraBars.LinkPersistInfo(this.barButtonItem2),
-            new DevExpress.XtraBars.LinkPersistInfo(this.barButtonItem3)});
+            new DevExpress.XtraBars.LinkPersistInfo(DevExpress.XtraBars.BarLinkUserDefines.PaintStyle, this.barButtonItem1, DevExpress.XtraBars.BarItemPaintStyle.CaptionGlyph),
+            new DevExpress.XtraBars.LinkPersistInfo(DevExpress.XtraBars.BarLinkUserDefines.PaintStyle, this.barButtonItem2, DevExpress.XtraBars.BarItemPaintStyle.CaptionGlyph),
+            new DevExpress.XtraBars.LinkPersistInfo(DevExpress.XtraBars.BarLinkUserDefines.PaintStyle, this.btnDelete, DevExpress.XtraBars.BarItemPaintStyle.CaptionGlyph)});
             this.bar2.OptionsBar.MultiLine = true;
             this.bar2.OptionsBar.UseWholeRow = true;
             this.bar2.Text = "Main menu";
@@ -84,20 +84,30 @@
             // barButtonItem1
             // 
             this.barButtonItem1.Caption = "Thêm";
+            this.barButtonItem1.Glyph = global::QLKHO.Properties.Resources._new;
             this.barButtonItem1.Id = 0;
             this.barButtonItem1.Name = "barButtonItem1";
             // 
             // barButtonItem2
             // 
             this.barButtonItem2.Caption = "Sửa";
+            this.barButtonItem2.Glyph = global::QLKHO.Properties.Resources.Add;
             this.barButtonItem2.Id = 1;
             this.barButtonItem2.Name = "barButtonItem2";
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Caption = "Xóa";
+            this.btnDelete.Glyph = global::QLKHO.Properties.Resources.del;
+            this.btnDelete.Id = 2;
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.ItemClick += new DevExpress.XtraBars.ItemClickEventHandler(this.btnDelete_ItemClick);
             // 
             // barDockControlTop
             // 
             this.barDockControlTop.Dock = System.Windows.Forms.DockStyle.Top;
             this.barDockControlTop.Location = new System.Drawing.Point(0, 0);
-            this.barDockControlTop.Size = new System.Drawing.Size(827, 24);
+            this.barDockControlTop.Size = new System.Drawing.Size(827, 42);
             // 
             // barDockControlBottom
             // 
@@ -108,33 +118,27 @@
             // barDockControlLeft
             // 
             this.barDockControlLeft.Dock = System.Windows.Forms.DockStyle.Left;
-            this.barDockControlLeft.Location = new System.Drawing.Point(0, 24);
-            this.barDockControlLeft.Size = new System.Drawing.Size(0, 399);
+            this.barDockControlLeft.Location = new System.Drawing.Point(0, 42);
+            this.barDockControlLeft.Size = new System.Drawing.Size(0, 381);
             // 
             // barDockControlRight
             // 
             this.barDockControlRight.Dock = System.Windows.Forms.DockStyle.Right;
-            this.barDockControlRight.Location = new System.Drawing.Point(827, 24);
-            this.barDockControlRight.Size = new System.Drawing.Size(0, 399);
+            this.barDockControlRight.Location = new System.Drawing.Point(827, 42);
+            this.barDockControlRight.Size = new System.Drawing.Size(0, 381);
             // 
-            // barButtonItem3
+            // grdGroup
             // 
-            this.barButtonItem3.Caption = "Xóa";
-            this.barButtonItem3.Id = 2;
-            this.barButtonItem3.Name = "barButtonItem3";
-            // 
-            // gridControl1
-            // 
-            this.gridControl1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.gridControl1.Location = new System.Drawing.Point(0, 24);
-            this.gridControl1.MainView = this.gridView1;
-            this.gridControl1.MenuManager = this.barManager1;
-            this.gridControl1.Name = "gridControl1";
-            this.gridControl1.RepositoryItems.AddRange(new DevExpress.XtraEditors.Repository.RepositoryItem[] {
+            this.grdGroup.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.grdGroup.Location = new System.Drawing.Point(0, 42);
+            this.grdGroup.MainView = this.gridView1;
+            this.grdGroup.MenuManager = this.barManager1;
+            this.grdGroup.Name = "grdGroup";
+            this.grdGroup.RepositoryItems.AddRange(new DevExpress.XtraEditors.Repository.RepositoryItem[] {
             this.repositoryItemLookUpEdit1});
-            this.gridControl1.Size = new System.Drawing.Size(827, 399);
-            this.gridControl1.TabIndex = 4;
-            this.gridControl1.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.grdGroup.Size = new System.Drawing.Size(827, 381);
+            this.grdGroup.TabIndex = 4;
+            this.grdGroup.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
             this.gridView1});
             // 
             // gridView1
@@ -144,15 +148,21 @@
             this.gridColumn2,
             this.gridColumn3,
             this.gridColumn4});
-            this.gridView1.GridControl = this.gridControl1;
+            this.gridView1.GridControl = this.grdGroup;
             this.gridView1.Name = "gridView1";
+            this.gridView1.OptionsView.NewItemRowPosition = DevExpress.XtraGrid.Views.Grid.NewItemRowPosition.Top;
             this.gridView1.OptionsView.ShowGroupPanel = false;
+            this.gridView1.RowUpdated += new DevExpress.XtraGrid.Views.Base.RowObjectEventHandler(this.gridView1_RowUpdated);
+            this.gridView1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.gridView1_KeyPress);
             // 
             // gridColumn1
             // 
             this.gridColumn1.Caption = "Mã ";
             this.gridColumn1.FieldName = "Id_Dis";
             this.gridColumn1.Name = "gridColumn1";
+            this.gridColumn1.OptionsColumn.AllowEdit = false;
+            this.gridColumn1.OptionsColumn.AllowFocus = false;
+            this.gridColumn1.OptionsColumn.ReadOnly = true;
             this.gridColumn1.Visible = true;
             this.gridColumn1.VisibleIndex = 0;
             // 
@@ -160,10 +170,22 @@
             // 
             this.gridColumn2.Caption = "Danh mục";
             this.gridColumn2.ColumnEdit = this.repositoryItemLookUpEdit1;
-            this.gridColumn2.FieldName = "NameCat";
+            this.gridColumn2.FieldName = "ID_Cat";
             this.gridColumn2.Name = "gridColumn2";
             this.gridColumn2.Visible = true;
             this.gridColumn2.VisibleIndex = 1;
+            // 
+            // repositoryItemLookUpEdit1
+            // 
+            this.repositoryItemLookUpEdit1.AutoHeight = false;
+            this.repositoryItemLookUpEdit1.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            this.repositoryItemLookUpEdit1.Columns.AddRange(new DevExpress.XtraEditors.Controls.LookUpColumnInfo[] {
+            new DevExpress.XtraEditors.Controls.LookUpColumnInfo("Id_Dis", "Mã"),
+            new DevExpress.XtraEditors.Controls.LookUpColumnInfo("NameCat", "Tên")});
+            this.repositoryItemLookUpEdit1.DisplayMember = "NameCat";
+            this.repositoryItemLookUpEdit1.Name = "repositoryItemLookUpEdit1";
+            this.repositoryItemLookUpEdit1.ValueMember = "ID_Cat";
             // 
             // gridColumn3
             // 
@@ -181,28 +203,22 @@
             this.gridColumn4.Visible = true;
             this.gridColumn4.VisibleIndex = 3;
             // 
-            // repositoryItemLookUpEdit1
-            // 
-            this.repositoryItemLookUpEdit1.AutoHeight = false;
-            this.repositoryItemLookUpEdit1.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
-            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
-            this.repositoryItemLookUpEdit1.Name = "repositoryItemLookUpEdit1";
-            // 
             // frmGroupItem
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(827, 423);
-            this.Controls.Add(this.gridControl1);
+            this.Controls.Add(this.grdGroup);
             this.Controls.Add(this.barDockControlLeft);
             this.Controls.Add(this.barDockControlRight);
             this.Controls.Add(this.barDockControlBottom);
             this.Controls.Add(this.barDockControlTop);
+            this.KeyPreview = true;
             this.Name = "frmGroupItem";
             this.Text = "frmGroupItem";
             this.Load += new System.EventHandler(this.frmGroupItem_Load);
             ((System.ComponentModel.ISupportInitialize)(this.barManager1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.grdGroup)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.gridView1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.repositoryItemLookUpEdit1)).EndInit();
             this.ResumeLayout(false);
@@ -219,8 +235,8 @@
         private DevExpress.XtraBars.BarDockControl barDockControlRight;
         private DevExpress.XtraBars.BarButtonItem barButtonItem1;
         private DevExpress.XtraBars.BarButtonItem barButtonItem2;
-        private DevExpress.XtraBars.BarButtonItem barButtonItem3;
-        private DevExpress.XtraGrid.GridControl gridControl1;
+        private DevExpress.XtraBars.BarButtonItem btnDelete;
+        private DevExpress.XtraGrid.GridControl grdGroup;
         private DevExpress.XtraGrid.Views.Grid.GridView gridView1;
         private DevExpress.XtraGrid.Columns.GridColumn gridColumn1;
         private DevExpress.XtraGrid.Columns.GridColumn gridColumn2;
--- QLKHO/QLKHO/FORM/MANAGEMENT/frmGroupItem.cs
+++ QLKHO/QLKHO/FORM/MANAGEMENT/frmGroupItem.cs
@@ -6,19 +6,167 @@ using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
 using DevExpress.XtraEditors;
-
+using QLKHO.DATAOBJECT;
 namespace QLKHO.FORM.MANAGEMENT
 {
     public partial class frmGroupItem : COREBASE.FORM.BASEFORM
     {
-        public frmGroupItem()
+        CatalogDao DaoCat;
+        GroupItemDao DaoGroup;
+        public frmGroupItem(COREBASE.COMMAND.Config.ConfigItem _ConfItem)
         {
+            _ConfigItem = _ConfItem;
             InitializeComponent();
+            DaoCat = new CatalogDao(_ConfigItem);
+            DaoGroup = new GroupItemDao(_ConfigItem);
         }
 
         private void frmGroupItem_Load(object sender, EventArgs e)
         {
+            LoadGird();
+        }
+        public void LoadGird()
+        {
+            grdGroup.DataSource = DaoGroup.GetList();
+            LoadCatalog();
+        }
+        private void LoadCatalog() {
+            repositoryItemLookUpEdit1.DataSource = DaoCat.GetListCombo();
+        }
+        private void Insert(DataRow row)
+        {
+    //"@Name",
+    //                "@Crt_Dt",
+    //                "@Crt_By",
+    //                "@Is_Del",
+    //                "@Remark",
+    //                "@Id_Cat"
+            try
+            {
+                object[] arrParaValue = new object[] {
+                    row["Name"],
+                    DateTime.Now,
+                    _ConfigItem.Login_UserName,
+                    0,
+                    row["Remark"],
+                    row["Id_Cat"]
+                };
+                DaoGroup.Insert(arrValue: arrParaValue);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        private void Delete(int _id)
+        {
+            try
+            {
+                DaoGroup.Delete(_id);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
 
+        }
+
+
+        private bool Update(DataRow row)
+        {
+            //"@Id",
+            //        "@Name",
+            //        "@Mod_Dt",
+            //        "@Mod_By",
+            //        "@Is_Del",
+            //        "@Remark",
+            //        "@Id_Cat"
+            try
+            {
+                object[] arrParaValue = new object[] {
+                    row["Id"],
+                    row["Name"],
+                    DateTime.Now,
+                    _ConfigItem.Login_UserName,
+                    0,
+
+                    row["Remark"],
+                    row["Id_Cat"]
+                };
+                DaoGroup.Update(arrValue: arrParaValue);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+        private void gridView1_RowUpdated(object sender, DevExpress.XtraGrid.Views.Base.RowObjectEventArgs e)
+        {
+            try
+            {
+
+                DataTable Tmp = (DataTable)(grdGroup.DataSource);
+                for (int i = 0; i < Tmp.Rows.Count; i++)
+                {
+                    DataRow dr = Tmp.Rows[i];
+                    if (isModifedRow(dr))
+                    {
+                        Update(dr);
+                    }
+                    if (isNewRow(dr))
+                    {
+                        Insert(dr);
+                    }
+                    if (isDeletedRow(dr))
+                    {
+                        Delete((int)GetOriginalItemData(dr, "Id"));
+                    }
+                }
+                LoadGird();
+            }
+            catch (Exception ex)
+            {
+                //TODOL Ghi log cho nay
+                throw;
+            }
+        }
+
+        private void gridView1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar.Equals((char)System.Windows.Forms.Keys.Delete))
+            {
+                try
+                {
+                    int[] _IndexRowSelected = gridView1.GetSelectedRows();
+                    int _CurIndexRow = _IndexRowSelected[0];
+                    DataTable tmp = (DataTable)grdGroup.DataSource;
+                    Delete(CnvToInt32(tmp.Rows[_CurIndexRow]["Id"]));
+                    LoadGird();
+                }
+                catch (Exception ex)
+                {
+                    //TODO: Ghi log tai day nhe
+                    throw ex;
+                }
+            }
+        }
+
+        private void btnDelete_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            try
+            {
+                int[] _IndexRowSelected = gridView1.GetSelectedRows();
+                int _CurIndexRow = _IndexRowSelected[0];
+                DataTable tmp = (DataTable)grdGroup.DataSource;
+                Delete(CnvToInt32(tmp.Rows[_CurIndexRow]["Id"]));
+                LoadGird();
+            }
+            catch (Exception ex)
+            {
+                //TODO: Ghi log tai day nhe
+                throw ex;
+            }
         }
 
   
