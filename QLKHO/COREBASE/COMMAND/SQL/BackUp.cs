541934bce55a7bcd2cb53004756ce0cd4feb2a58
ntxuan2401 <ntxuan2401@4f51cf1c-0b8c-b79f-ed1e-ba211a99c7d2>
2010-10-16 17:07:58 +0000

    edit la thong tin nay; cai nay da chay duoc hoan chinh roi do

--- QLKHO/COREBASE/COMMAND/SQL/BackUp.cs
+++ QLKHO/COREBASE/COMMAND/SQL/BackUp.cs
@@ -4,40 +4,90 @@ using System.Linq;
 using System.Text;
 using Microsoft.SqlServer.Management.Smo;
 using System.Windows.Forms;
+using Microsoft.SqlServer.Management.Common;
 
 namespace COREBASE.COMMAND.SQL
 {
     public class BACKUP
     {
-        private static Server srvSql =null;
-
-        public static void CreateBackUp(string p_FileName, COMMAND.Config.ConfigItem p_confItem)
+       private static Form objFormCall = null;
+        /// <summary>
+        /// Phuong thuc thuc hien backup du lieu
+        /// </summary>
+        /// <param name="p_FileName"></param>
+        /// <param name="p_confItem"></param>
+        /// <param name="p_objCall"></param>
+        public static void CreateBackUp(string p_FileName, COMMAND.Config.ConfigItem p_confItem, object p_objCall)
         {
-
-            // If there was a SQL connection created
-            srvSql = new Server(p_confItem.StrServerName);
-            if (srvSql != null)
+            try
             {
-                // If the user has chosen a path where to save the backup file
-                // Create a new backup operation
-                Backup bkpDatabase = new Backup();
-                // Set the backup type to a database backup
-                bkpDatabase.Action = BackupActionType.Database;
-                // Set the database that we want to perform a backup on
-                bkpDatabase.Database = p_confItem.StrDBName;
-                // Set the backup device to a file
-                BackupDeviceItem bkpDevice = new BackupDeviceItem(p_FileName, DeviceType.File);
-                // Add the backup device to the backup
-                bkpDatabase.Devices.Add(bkpDevice);
-                // Perform the backup
-                bkpDatabase.SqlBackup(srvSql);
-            }
-            else
-            {
-                // There was no connection established; probably the Connect button was not clicked
-                MessageBox.Show("A connection to a SQL server was not established.", "Not Connected to Server", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
-            }
+                objFormCall = (Form)p_objCall;
+                objFormCall.Enabled = false;
+                objFormCall.Controls["groupControl1"].Controls["lblStatus"].Text = COMMAND.MessageUtils.Message.GetMessageById("BKRS_E_005");// "Dang khoi dong qua trinh sao luu du lieu";
+                //WriteToLogAndConsole("Backup the {0} database!", databaseName);
+                //khai bao thong tin device backup
+                BackupDeviceItem deviceItem = new BackupDeviceItem(p_FileName, DeviceType.File);
+                //thiet lap thong tin sql backup
+                Backup sqlBackup = new Backup();
+                sqlBackup.Action = BackupActionType.Database;
+                sqlBackup.Database = p_confItem.StrDBName;
+                sqlBackup.BackupSetDescription = "ArchiveDataBase:" + DateTime.Now.ToShortDateString();
+                sqlBackup.BackupSetName = "Archive";
+                sqlBackup.Initialize = true;
+                sqlBackup.Checksum = true;
+                sqlBackup.ContinueAfterError = true;
+                sqlBackup.Devices.Add(deviceItem);
+                sqlBackup.Incremental = false;
+                sqlBackup.ExpirationDate = DateTime.Now.AddDays(3);
+                sqlBackup.LogTruncation = BackupTruncateLogType.Truncate;
+                sqlBackup.FormatMedia = false;
+                sqlBackup.PercentComplete += new PercentCompleteEventHandler(sqlBackup_PercentComplete);
+                sqlBackup.Complete += new ServerMessageEventHandler(sqlBackup_Complete);
 
+                //thiet lap thong tin server va connection den server
+                ServerConnection connection = new ServerConnection(p_confItem.StrServerName, p_confItem.StrUserID, p_confItem.StrPassword);
+                Server sqlServer = new Server(connection);
+                //Database db = sqlServer.Databases[p_confItem.StrDBName];
+                objFormCall.Controls["groupControl1"].Controls["lblStatus"].Text = COMMAND.MessageUtils.Message.GetMessageById("BKRS_E_004");
+                sqlBackup.SqlBackup(sqlServer);
+            }
+            catch (Exception ex)
+            {
+                if (objFormCall != null)
+                {
+                    objFormCall.Controls["groupControl1"].Controls["lblStatus"].Text = COMMAND.MessageUtils.Message.GetMessageById("BKRS_E_000");
+                    objFormCall.Enabled = true;
+                    LogUtils.Logger.AppLog.Debug(objFormCall.Name, p_confItem.Login_UserName,
+                        objFormCall.Controls["groupControl1"].Controls["lblStatus"].Text, ex);
+                }
+            }
+        }
+        /// <summary>
+        /// event khi thuc hien backup thanh cong
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        static void sqlBackup_Complete(object sender, ServerMessageEventArgs e)
+        {
+            if (objFormCall != null)
+            {
+                objFormCall.Controls["groupControl1"].Controls["lblStatus"].Text = COMMAND.MessageUtils.Message.GetMessageById("BKRS_E_003");//string.Format("Sao luu du lieu: {0}%", e.ToString());
+                objFormCall.Enabled = true;
+            }
+            // WriteToLogAndConsole(e.ToString() + "% Complete");
+        }
+        /// <summary>
+        /// event trong qua trinh thuc hien back up
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        static void sqlBackup_PercentComplete(object sender, PercentCompleteEventArgs e)
+        {
+            if (objFormCall != null)
+            {
+                objFormCall.Controls["groupControl1"].Controls["lblStatus"].Text = string.Format(COMMAND.MessageUtils.Message.GetMessageById("BKRS_E_001"), e.Percent);
+            }
+            //WriteToLogAndConsole(e.Percent.ToString() + "% Complete");
         }
 
     }
--- QLKHO/COREBASE/COMMAND/SQL/Restore.cs
+++ QLKHO/COREBASE/COMMAND/SQL/Restore.cs
@@ -4,44 +4,93 @@ using System.Linq;
 using System.Text;
 using Microsoft.SqlServer.Management.Smo;
 using System.Windows.Forms;
+using Microsoft.SqlServer.Management.Common;
 
 
 namespace COREBASE.COMMAND.SQL
 {
     public class RESTORE
     {
-        private static Server srvSql = null;
+        private static Form objFormCall = null;
 
-        public static void CreateRestore(string p_FileName, COMMAND.Config.ConfigItem p_confItem)
+        /// <summary>
+        /// phuc thuc phuc hoi du lieu , voi duong dan mac dinh se duoc tai cho thu muc cua chuong trinh
+        /// </summary>
+        /// <param name="p_FileName"></param>
+        /// <param name="p_confItem"></param>
+        public static void CreateRestore(string p_FileName, COMMAND.Config.ConfigItem p_confItem, object p_objCall)
         {
-
-            // If there was a SQL connection created
-            srvSql = new Server(p_confItem.StrServerName);
-            if (srvSql != null)
+            try
             {
-                // Create a new database restore operation
-                Restore rstDatabase = new Restore();
-                // Set the restore type to a database restore
-                rstDatabase.Action = RestoreActionType.Database;
-                // Set the database that we want to perform the restore on
-                rstDatabase.Database = p_confItem.StrDBName;
-                // Set the backup device from which we want to restore, to a file
-                BackupDeviceItem bkpDevice = new BackupDeviceItem(p_FileName, DeviceType.File);
-                // Add the backup device to the restore type
-                rstDatabase.Devices.Add(bkpDevice);
-                // If the database already exists, replace it
-                rstDatabase.ReplaceDatabase = true;
-                // Perform the restore
-                rstDatabase.SqlRestore(srvSql);
+                objFormCall = (Form)p_objCall;
+                objFormCall.Enabled = false;
+                objFormCall.Controls["groupControl1"].Controls["lblStatus"].Text = COMMAND.MessageUtils.Message.GetMessageById("BKRS_E_015");
+                string l_PathApp = Application.StartupPath + "\\App_Data";
+                if (!System.IO.Directory.Exists(l_PathApp))
+                {
+                    System.IO.Directory.CreateDirectory(l_PathApp);
+                }
+
+                //khai bao thiet bi du lieu phuc hoi
+                BackupDeviceItem deviceItem = new BackupDeviceItem(p_FileName, DeviceType.File);
+                //khai bao doi tuong server 
+                ServerConnection connection = new ServerConnection(p_confItem.StrServerName, p_confItem.StrUserName, p_confItem.StrPassword);
+                Server sqlServer = new Server(connection);
+                //khai bao doi tuong database
+                Database db = sqlServer.Databases[p_confItem.StrDBName];
+                if (db != null)
+                    sqlServer.KillAllProcesses(p_confItem.StrDBName);
+                //khai bao doi tuong phuc hoi du lieu
+                Restore sqlRestore = new Restore();
+                sqlRestore.Devices.Add(deviceItem);
+                sqlRestore.Database = p_confItem.StrDBName;
+                sqlRestore.Action = RestoreActionType.Database;
+                String dataFileLocation = l_PathApp + "\\" + p_confItem.StrDBName + ".mdf";
+                String logFileLocation = l_PathApp + "\\" + p_confItem.StrDBName + "_Log.ldf";
+                RelocateFile rf = new RelocateFile(p_confItem.StrDBName, dataFileLocation);
+                sqlRestore.RelocateFiles.Add(new RelocateFile(p_confItem.StrDBName, dataFileLocation));
+                sqlRestore.RelocateFiles.Add(new RelocateFile(p_confItem.StrDBName + "_log", logFileLocation));
+                sqlRestore.ReplaceDatabase = true;
+                sqlRestore.Complete += new ServerMessageEventHandler(sqlRestore_Complete);
+                sqlRestore.PercentCompleteNotification = 10;
+                sqlRestore.PercentComplete += new PercentCompleteEventHandler(sqlRestore_PercentComplete);
+                objFormCall.Controls["groupControl1"].Controls["lblStatus"].Text = COMMAND.MessageUtils.Message.GetMessageById("BKRS_E_014");
+                sqlRestore.SqlRestore(sqlServer);
+                db = sqlServer.Databases[p_confItem.StrDBName];
+                db.SetOnline();
+                sqlServer.Refresh();
+
             }
-            else
+            catch (Exception ex)
             {
-                // There was no connection established; probably the Connect button was not clicked
-                MessageBox.Show("A connection to a SQL server was not established.", "Not Connected to Server", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                if (objFormCall != null)
+                {
+                    objFormCall.Controls["groupControl1"].Controls["lblStatus"].Text = COMMAND.MessageUtils.Message.GetMessageById("BKRS_E_010");
+                    objFormCall.Enabled = true;
+                    LogUtils.Logger.AppLog.Debug(objFormCall.Name, p_confItem.Login_UserName,
+                        objFormCall.Controls["groupControl1"].Controls["lblStatus"].Text, ex);
+                }
             }
-
-
         }
 
+
+        static void sqlRestore_PercentComplete(object sender, PercentCompleteEventArgs e)
+        {
+            if (objFormCall != null)
+            {
+                objFormCall.Controls["groupControl1"].Controls["lblStatus"].Text = string.Format(COMMAND.MessageUtils.Message.GetMessageById("BKRS_E_011"), e.Percent);
+            }
+            //WriteToLogAndConsole(e.Percent.ToString() + "% Complete");
+        }
+
+        static void sqlRestore_Complete(object sender, ServerMessageEventArgs e)
+        {
+            if (objFormCall != null)
+            {
+                objFormCall.Controls["groupControl1"].Controls["lblStatus"].Text = COMMAND.MessageUtils.Message.GetMessageById("BKRS_E_013");//string.Format("Sao luu du lieu: {0}%", e.ToString());
+                objFormCall.Enabled = true;
+            }
+            // WriteToLogAndConsole(e.ToString() + "% Complete");
+        }
     }
 }
